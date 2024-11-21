2fc3a145af9d91b83f49a3df67b09a1cc913467d
Arze-666 <silenceigor2020@gmail.com>
2024-11-21 09:42:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function buyNow(productName, price) {
+    alert(`Você escolheu ${productName} por R$${price.toFixed(2)}! Redirecionando para o pagamento...`);
+    // Aqui você pode integrar um sistema de pagamento, como PayPal ou Stripe.
+}
